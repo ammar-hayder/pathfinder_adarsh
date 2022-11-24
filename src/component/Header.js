afece5016de3bfd8253ec1cb2c5f4fab13a2996e
azamkhan <azamkhan0087@gmail.com>
2022-11-24 21:28:58 +0530

    4th

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,7 @@
 // import logo from '../image/logo1.png';
 import 'aos/dist/aos.css';
 // import {NavLink} from 'react-router-dom';
+import logo2 from '../image/logo2.png'
 
 const Header = () => {
 
@@ -34,6 +35,7 @@ const Header = () => {
               <h3>Welcome to ADARSH PARK HEIGHTS</h3>
               <hr />
               <h4>Gunjur, East Banglore</h4>
+              <img className='img-f' src={logo2} alt="logo2" />
               <br/>
               
             </div>
